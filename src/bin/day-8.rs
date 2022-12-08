80913634cd68348c45cb0367e94b1a803fa0a6c8
Mark Romano <mark.romano@laitram.com>
2022-12-08 13:19:24 -0600

    Complete 8.2.

--- src/bin/day-8.rs
+++ src/bin/day-8.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 fn main() {
     let input = get_input();
     println!("Got {} characters of input.", input.len());
@@ -5,49 +7,52 @@ fn main() {
     let grid = get_grid(&input);
     println!("Grid: {:?}", grid);
 
-    let visible_count = get_visible_count(&grid);
-    println!("Visible count: {:?}", visible_count);
+    let high_score = get_high_score(&grid);
+    println!("High score: {:?}", high_score);
 }
 
-fn get_visible_count(grid :&Vec<Vec<u32>>) -> u32{
-    let mut count = 0;
+fn get_high_score(grid :&Vec<Vec<u32>>) -> u32{
+    let mut high_score = 0;
     for x in 0..grid.len() {
         for y in 0..grid[x].len() {
-            if is_visible(&x, &y, &grid) {
-                count += 1;
-            }
+            let current_score = get_score(&x, &y, &grid);
+            high_score = cmp::max(high_score, current_score);
         }
     }
-    return count;
+    return high_score;
 }
 
-fn is_visible(x :&usize, y :&usize, grid :&Vec<Vec<u32>>) -> bool{
-    return is_visible_in_direction(x, y, grid, 0, -1) ||
-           is_visible_in_direction(x, y, grid, 0, 1) ||
-           is_visible_in_direction(x, y, grid, -1, 0) ||
-           is_visible_in_direction(x, y, grid, 1, 0);
+fn get_score(x :&usize, y :&usize, grid :&Vec<Vec<u32>>) -> u32{
+    let mut total_score = 1;
+    total_score *= get_score_in_direction(x, y, grid, 0, -1);
+    total_score *= get_score_in_direction(x, y, grid, 0, 1);
+    total_score *= get_score_in_direction(x, y, grid, -1, 0);
+    total_score *= get_score_in_direction(x, y, grid, 1, 0);
+    println!("Score [{},{}]: {}", x, y, total_score);
+    return total_score;
 }
 
-fn is_visible_in_direction(x :&usize, y :&usize, grid :&Vec<Vec<u32>>, x_offset :i8, y_offset :i8) -> bool{
+fn get_score_in_direction(x :&usize, y :&usize, grid :&Vec<Vec<u32>>, x_offset :i8, y_offset :i8) -> u32{
     let tree = grid[*x][*y];
     let max_x = grid.len();
     let max_y = grid[0].len();
     let mut current_x = adjust(&(x+1), &x_offset);
     let mut current_y = adjust(&(y+1), &y_offset);
+    let mut score = 0;
 
     while current_x > usize::MIN && current_x <= max_x && current_y > usize::MIN && current_y <= max_y {
+        score += 1;
         let blocking_tree = grid[current_x-1][current_y-1];
         if blocking_tree >= tree {
-            return false
+            return score;
         }
         current_x = adjust(&current_x, &x_offset);
         current_y = adjust(&current_y, &y_offset);
     }
-    return true;
+    return score;
 }
 
 fn adjust(index :&usize, offset :&i8) -> usize {
-    println!("Adjust: {:?},{:?}", index, offset);
     let abs_offset = offset.unsigned_abs();
     if offset >= &0 {
         return index + usize::from(abs_offset);
